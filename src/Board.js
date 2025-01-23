01fb58d5310477641afa7323dd1dbffd6bb70c59
NolanS55 <97527945+NolanS55@users.noreply.github.com>
2025-01-23 12:19:14 -0500

    MCTS Fix

--- src/Board.js
+++ src/Board.js
@@ -7,187 +7,196 @@ import './Board.css'
 class connectFour {
     constructor() {
         this.boardState = {
-            board : [[0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0]],
-            player : 1,
-            tempPlay : 2,
-            winner : 0,
-            gameOver : false
-        }
+            board : [[0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]],
+            player: 1,
+            tempPlay: 2,
+            winner: 0,
+            gameOver: false
+        };
     }
-                                    
+
+    // Return the current board state
     getBoardState() {
-        return this.boardState
+        return this.boardState;
     }
-                                    
+
+    // Set a new board state
     setBoardState(boardState) {
-        this.boardState = boardState
+        this.boardState = boardState;
     }
 
+    // Clone the board state to prevent mutation
     setBoard(board) {
-        this.boardState.board = structuredClone(board)
+        this.boardState.board = structuredClone(board);
     }
 
+    // Deep clone the board state
     deepClone() {
         return {
-            board : this.boardState.board,
-            player : this.boardState.player,
-            tempPlay : this.boardState.tempPlay,
-            winner : this.boardState.winner,
-            gameOver : this.boardState.gameOver       
-        }
+            board: this.boardState.board,
+            player: this.boardState.player,
+            tempPlay: this.boardState.tempPlay,
+            winner: this.boardState.winner,
+            gameOver: this.boardState.gameOver
+        };
     }
 
+    // Return the winner of the game
     getWinner() {
-        return this.boardState.winner
+        return this.boardState.winner;
     }
 
+    // Get all available moves (columns that are not full)
     getMoves() {
-        let moves = []
-        for(let i = 0; i < 7; i++) {
-            if(this.boardState.board[0][i] === 0) {
-                moves.push(i)
+        let moves = [];
+        for (let i = 0; i < 7; i++) {
+            if (this.boardState.board[0][i] === 0) {
+                moves.push(i);
             }
         }
-        return moves
+        return moves;
     }
 
-    move = (col) => {
-        for(let i = 5; i >= 0; i--) {
-            if(this.boardState.board[i][col] === 0) {
-                let tempBoard = [...this.boardState.board]
-                tempBoard[i][col] = this.boardState.player
-                this.boardState.board = tempBoard
-                let temperplay = this.boardState.player
-                this.boardState.player = this.boardState.tempPlay
-                this.boardState.tempPlay = temperplay
-                break
+    // Perform a move (drop a disc in a column)
+    move(col) {
+        for (let i = 5; i >= 0; i--) {
+            if (this.boardState.board[i][col] === 0) {
+                let tempBoard = [...this.boardState.board];
+                tempBoard[i][col] = this.boardState.player;
+                this.boardState.board = tempBoard;
+                let tempPlayer = this.boardState.player;
+                this.boardState.player = this.boardState.tempPlay;
+                this.boardState.tempPlay = tempPlayer;
+                break;
             }
         }
-        
     }
 
-    checkVert = (board) => {
-        let tiles = 0
-        let check = 0
-        let checkHold = 0
-        for(let i = 0; i < 7; i++) {
-            for(let j = 0;j < 6; j++) {
-                check = board[j][i]
-                if(check !== 0) {
-                    tiles++
+    // Check for a vertical win
+    checkVert(board) {
+        let tiles = 0;
+        let check = 0;
+        let checkHold = 0;
+        for (let i = 0; i < 7; i++) {
+            for (let j = 0; j < 6; j++) {
+                check = board[j][i];
+                if (check !== 0) {
+                    tiles++;
                 }
-                if(check !== checkHold) {
-                    tiles = 0
+                if (check !== checkHold) {
+                    tiles = 0;
                 }
-                if(tiles === 3) {
-                    return true
+                if (tiles === 3) {
+                    return true;
                 }
-                checkHold = board[j][i]
+                checkHold = board[j][i];
             }
-            tiles = 0
-            check = 0
-            checkHold = 0
+            tiles = 0;
+            check = 0;
+            checkHold = 0;
         }
-        return false
+        return false;
     }
 
-    checkHorz = (boardRow) => {
-        let tiles = 0
-        let check = 0
-        let checkHold = 0
-        for(let i = 0; i < 7; i++) {
-            check = boardRow[i]
-            if(check !== 0) {
-                tiles++
+    // Check for a horizontal win
+    checkHorz(boardRow) {
+        let tiles = 0;
+        let check = 0;
+        let checkHold = 0;
+        for (let i = 0; i < 7; i++) {
+            check = boardRow[i];
+            if (check !== 0) {
+                tiles++;
             }
-            if(check !== checkHold) {
-                tiles = 0
+            if (check !== checkHold) {
+                tiles = 0;
             }
-            if(tiles === 3) {
-                return true
+            if (tiles === 3) {
+                return true;
             }
-            checkHold = boardRow[i]
+            checkHold = boardRow[i];
         }
-        return false
+        return false;
     }
 
-    checkDiag = (board, row, column) => {
-        var result = false;
-
-        if(board[row][column] !== 0) {
-            // there are four possible directions of a win
-            // if the top right contains a possible win
-            if(row - 3 > -1 && column + 3 < 7) {
+    // Check for a diagonal win
+    checkDiag(board, row, column) {
+        let result = false;
+        if (board[row][column] !== 0) {
+            // Check top-right diagonal
+            if (row - 3 >= 0 && column + 3 < 7) {
                 result = board[row][column] === board[row - 1][column + 1] &&
                          board[row][column] === board[row - 2][column + 2] &&
-                         board[row][column] === board[row - 3][column + 3]; 
+                         board[row][column] === board[row - 3][column + 3];
             }
-            // if the bottom right contains possible win
-            if(row + 3 < 6 && column + 3 < 7) {
+            // Check bottom-right diagonal
+            if (row + 3 < 6 && column + 3 < 7) {
                 result = board[row][column] === board[row + 1][column + 1] &&
                          board[row][column] === board[row + 2][column + 2] &&
-                         board[row][column] === board[row + 3][column + 3]; 
+                         board[row][column] === board[row + 3][column + 3];
             }
-            // if the bottom left contains possible win
-            if(row + 3 < 6 && column - 3 > -1) {
+            // Check bottom-left diagonal
+            if (row + 3 < 6 && column - 3 >= 0) {
                 result = board[row][column] === board[row + 1][column - 1] &&
                          board[row][column] === board[row + 2][column - 2] &&
-                         board[row][column] === board[row + 3][column - 3]; 
+                         board[row][column] === board[row + 3][column - 3];
             }
-            // if the top left contains a possible win
-            if(row - 3 > -1 && column - 3 > -1) {
+            // Check top-left diagonal
+            if (row - 3 >= 0 && column - 3 >= 0) {
                 result = board[row][column] === board[row - 1][column - 1] &&
                          board[row][column] === board[row - 2][column - 2] &&
-                         board[row][column] === board[row - 3][column - 3]; 
+                         board[row][column] === board[row - 3][column - 3];
             }
         }
-    
         return result;
     }
 
-    checkWin = () => {
-        for(let i = 0; i < 6; i++) {
-            if(this.checkHorz(this.boardState.board[i])) {
-                this.boardState.gameOver = true
-                this.boardState.winner  = this.boardState.player
-                return true
+    // Check for a win condition in the game (horizontal, vertical, or diagonal)
+    checkWin() {
+        for (let i = 0; i < 6; i++) {
+            if (this.checkHorz(this.boardState.board[i])) {
+                this.boardState.gameOver = true;
+                this.boardState.winner = this.boardState.player;
+                return true;
             }
         }
-        if(this.checkVert(this.boardState.board)) {
-            this.boardState.gameOver = true
-            this.boardState.winner  = this.boardState.player
-            return true
+        if (this.checkVert(this.boardState.board)) {
+            this.boardState.gameOver = true;
+            this.boardState.winner = this.boardState.player;
+            return true;
         }
-        for(let i = 0; i < 6; i++) {
-            for(let j = 0; j < 7; j++) {
-                if(this.checkDiag(this.boardState.board,i,j)) {
-                    this.boardState.gameOver = true
-                    this.boardState.winner  = this.boardState.player
-                    return true
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (this.checkDiag(this.boardState.board, i, j)) {
+                    this.boardState.gameOver = true;
+                    this.boardState.winner = this.boardState.player;
+                    return true;
                 }
             }
         }
-        return false
+        return false;
     }
-    reset = () => {
+
+    // Reset the game to the initial state
+    reset() {
         this.setBoardState({
-            board : [[0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0]],
-            player : 1,
-            tempPlay : 2,
-            winner : 0,
-            lastMove : -1,
-            gameOver : false
-        })
+            board: [[0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]],
+            player: 1,
+            tempPlay: 2,
+            winner: 0,
+            lastMove: -1,
+            gameOver: false
+        });
     }
 }
 var connectGame = new connectFour()
